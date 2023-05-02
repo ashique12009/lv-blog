384b36b7a6c9b7171ad1c935913bcaa191862752
Ashique <ashique12009@gmail.com>
2023-05-02 11:40:15 +0600

    role edit done

--- backend/app/Http/Controllers/RoleController.php
+++ backend/app/Http/Controllers/RoleController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
@@ -63,7 +63,13 @@ class RoleController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $role            = Role::find($id);
+        $permission      = Permission::get();
+        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
+            ->pluck('role_has_permissions.permission_id', 'role_has_permissions.permission_id')
+            ->all();
+
+        return view('roles.edit', compact('role', 'permission', 'rolePermissions'));
     }
 
     /**
@@ -71,7 +77,19 @@ class RoleController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $this->validate($request, [
+            'name'       => 'required',
+            'permission' => 'required',
+        ]);
+
+        $role       = Role::find($id);
+        $role->name = $request->input('name');
+        $role->save();
+
+        $role->syncPermissions($request->input('permission'));
+
+        return redirect()->route('roles.index')
+            ->with('success', 'Role updated successfully');
     }
 
     /**
